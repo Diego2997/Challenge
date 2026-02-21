597f1a1cb258f7ae0b8eded080f2c5f02411f679
Diego <diego.mercado1997@hotmail.com>
2026-02-21 08:46:58 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+        <p>Holaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+        <p>Holaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+        <p>Holaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+        <p>Holaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+        <p>Holaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+        <p>Holaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+      </div>
+        <p>Holaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+
+      {/* <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p> */}
+    </>
+  )
+}
+
+export default App
